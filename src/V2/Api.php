430a0b5721330162cf10885f7ca9324b260695c7
selistore <afriyiejo438@gmail.com>
2025-02-17 22:47:57 +0000

    Update Api.php

--- src/V2/Api.php
+++ src/V2/Api.php
@@ -3,43 +3,77 @@ namespace Selistore\PrintfulApi\V2;
 
 use Selistore\PrintfulApi\V2\Auth as PrintfulAuth;
 
+/**
+ * Class Api
+ *
+ * This class provides access to various API endpoints such as scopes, catalog, orders, countries, shipping, and stores.
+ * It extends the PrintfulAuth class for authentication.
+ */
 final class Api extends PrintfulAuth
 {
-  public function scopes() {
-    return new class{
-      use \Selistore\PrintfulApi\V2\Repository\Scopes;
-    };
-  }
+    /**
+     * Get the scopes repository.
+     *
+     * @return object Anonymous class using the Scopes trait.
+     */
+    public function scopes() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Repository\Scopes;
+        };
+    }
 
-  public function catalog() {
-    return new class {
-      use \Selistore\PrintfulApi\V2\Catalog;
-    };
-  }
+    /**
+     * Get the catalog repository.
+     *
+     * @return object Anonymous class using the Catalog trait.
+     */
+    public function catalog() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Catalog;
+        };
+    }
 
-  public function orders() {
-    return new class {
-      use \Selistore\PrintfulApi\V2\Orders;
-    };
-  }
+    /**
+     * Get the orders repository.
+     *
+     * @return object Anonymous class using the Orders trait.
+     */
+    public function orders() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Orders;
+        };
+    }
 
-  public function countries() {
-    return new class {
-      use \Selistore\PrintfulApi\V2\Repository\Countries;
-    };
-  }
+    /**
+     * Get the countries repository.
+     *
+     * @return object Anonymous class using the Countries trait.
+     */
+    public function countries() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Repository\Countries;
+        };
+    }
 
-  public function shipping() {
-    return new class {
-      use \Selistore\PrintfulApi\V2\Repository\ShippingRates;
-    };
-  }
+    /**
+     * Get the shipping rates repository.
+     *
+     * @return object Anonymous class using the ShippingRates trait.
+     */
+    public function shipping() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Repository\ShippingRates;
+        };
+    }
 
-  public function stores() {
-    return new class {
-      use \Selistore\PrintfulApi\V2\Repository\Stores;
-    };
-  }
-
-
+    /**
+     * Get the stores repository.
+     *
+     * @return object Anonymous class using the Stores trait.
+     */
+    public function stores() {
+        return new class {
+            use \Selistore\PrintfulApi\V2\Repository\Stores;
+        };
+    }
 }
